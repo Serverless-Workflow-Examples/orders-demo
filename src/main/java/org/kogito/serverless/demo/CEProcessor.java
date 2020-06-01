51e8836570b5d5772155be98905533543d361aad
Tihomir Surdilovic <tsurdilo@redhat.com>
2020-06-01 00:18:47 -0400

    adding functionality

---
+++ src/main/java/org/kogito/serverless/demo/CEProcessor.java
@@ -0,0 +1,46 @@
+/**
+ *  Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.kogito.serverless.demo;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cloudevents.json.Json;
+import io.cloudevents.v1.CloudEventBuilder;
+import io.cloudevents.v1.CloudEventImpl;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import java.net.URI;
+import java.util.UUID;
+
+public class CEProcessor implements Processor {
+
+    public void process(Exchange exchange) throws Exception {
+        String message = exchange.getIn().getBody(String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode orderMessageObj = mapper.readTree(message);
+
+        CloudEventImpl<JsonNode> orderCE =
+                CloudEventBuilder.<JsonNode>builder()
+                        .withId(UUID.randomUUID().toString())
+                        .withType("newOrderEvent")
+                        .withSource(URI.create("http://localhost:8080"))
+                        .withData(orderMessageObj)
+                        .build();
+        exchange.getIn().setBody(Json.encode(orderCE));
+    }
+}
---
+++ src/main/resources/META-INF/resources/orders.js
@@ -0,0 +1,172 @@
+var workflowsSource = new EventSource("/workflows/stream");
+workflowsSource.onmessage = function (event) {
+     addToWorkflowsTable(event.data);
+};
+var workflowsTable = $('#workflowinstancestable').DataTable({ searching: false, paging: false, info: false });
+var counter = 1;
+
+function addToWorkflowsTable(eventdata) {
+     var eventobj = JSON.parse(eventdata);
+     var processinstancestate = eventobj.kogitoProcessinstanceState
+     var processinstancestateword = "";
+     if (processinstancestate == 0) {
+          processinstancestateword = "Pending";
+     }
+     if (processinstancestate == 1) {
+          processinstancestateword = "Active";
+     }
+     if (processinstancestate == 2) {
+          processinstancestateword = "Completed";
+     }
+     if (processinstancestate == 3) {
+          processinstancestateword = "Aborted";
+     }
+     if (processinstancestate == 4) {
+          processinstancestateword = "Suspended";
+     }
+     if (processinstancestate == 5) {
+          processinstancestateword = "Error";
+     }
+
+     workflowsTable.row.add([
+          counter,
+          eventobj.data.processId,
+          eventobj.data.processName,
+          eventobj.kogitoProcessinstanceId,
+          processinstancestateword
+     ]).draw(false);
+
+     counter++;
+}
+
+var humandecisionsSource = new EventSource("/usertasks/stream");
+humandecisionsSource.onmessage = function (event) {
+     addToHumanDecisionsTable(event.data);
+
+
+     var usertaskData = JSON.parse(event.data);
+     if (usertaskData.kogitoUserTaskinstanceState == 'Ready') {
+          addToManagerApprovalTable(event.data);
+     }
+
+};
+var humandecisionsTable = $('#humandecisioninstancestable').DataTable({ searching: false, paging: false, info: false });
+var counterb = 1;
+
+function addToHumanDecisionsTable(eventdata) {
+     var eventobj = JSON.parse(eventdata);
+     humandecisionsTable.row.add([
+          counterb,
+          eventobj.kogitoProcessId,
+          eventobj.kogitoProcessinstanceId,
+          eventobj.kogitoUserTaskinstanceId,
+          eventobj.kogitoUserTaskinstanceState,
+          eventobj.source
+     ]).draw(false);
+
+     counterb++;
+}
+
+$("#creteorder").on('click', function () {
+     $.ajax({
+          url: 'http://localhost:8080/neworder',
+          type: "POST",
+          dataType: 'json',
+          headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json'
+          },
+          data: JSON.stringify($("#newOrderForm").serializeToJSON()),
+          success: function (result) {
+               console.log(result);
+          },
+          error: function (xhr, resp, text) {
+               console.log(xhr, resp, text);
+          }
+     })
+});
+
+var managerapprovalsTable = $('#managerapprovaltable').DataTable({
+     searching: false, paging: false, info: false,
+     "columnDefs": [{
+          "targets": -1,
+          "data": null,
+          "defaultContent": "<button id=\"managerApproveButton\">Aprove</button><button id=\"managerDenyButton\">Deny</button>"
+     }]
+});
+var counterc = 1;
+
+function addToManagerApprovalTable(eventdata) {
+     var eventobj = JSON.parse(eventdata);
+     managerapprovalsTable.row.add([
+          counterc,
+          eventobj.kogitoProcessinstanceId,
+          eventobj.kogitoUserTaskinstanceId,
+          eventobj.data.inputs.workflowdata.cost
+     ]).draw(false);
+
+     counterc++;
+}
+
+var managerDecisionStr = "";
+
+$('#managerapprovaltable tbody').on('click', '#managerApproveButton', function () {
+     var data = managerapprovalsTable.row($(this).parents('tr')).data();
+     doManagerApproval(data[1], data[2], 'Approved');
+     managerapprovalsTable.clear().draw();
+     managerDecisionStr = "Approved";
+});
+
+$('#managerapprovaltable tbody').on('click', '#managerDenyButton', function () {
+     var data = managerapprovalsTable.row($(this).parents('tr')).data();
+     doManagerApproval(data[1], data[2], 'Denied');
+     managerapprovalsTable.clear().draw();
+     managerDecisionStr = "Denied"
+});
+
+function doManagerApproval(processinstance, taskinstance, managerDecision) {
+     var managerDecisionData = {"decision" : managerDecision};
+     $.ajax({
+          url: 'http://localhost:8080/ordersworkflow/' + processinstance + '/managerApproval/' + taskinstance + '?user=managers&group=managers',
+          type: "POST",
+          dataType: 'json',
+          headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json'
+          },
+          data: JSON.stringify(managerDecisionData),
+          success: function (result) {
+               console.log(result);
+          },
+          error: function (xhr, resp, text) {
+               console.log(xhr, resp, text);
+          }
+     });
+}
+
+var orderdecisonsTable = $('#orderdecisionstable').DataTable({ searching: false, paging: false, info: false });
+var counterd = 1;
+
+function addToOrderDecisionsTable(eventdata) {
+     var eventobj = JSON.parse(eventdata);
+     var autoDecision = eventobj.data.decision;
+     var orderdec = "";
+     if(autoDecision == "NeedManagerApproval") {
+          orderdec = managerDecisionStr;
+     } else {
+          orderdec = autoDecision;
+     }
+
+     orderdecisonsTable.row.add([
+          counterd,
+          eventobj.kogitoProcessinstanceId,
+          orderdec
+     ]).draw(false);
+
+     counterd++;
+}
+
+var orderDecisionSource = new EventSource("/finalorders/stream");
+orderDecisionSource.onmessage = function (event) {
+     addToOrderDecisionsTable(event.data);
+};
